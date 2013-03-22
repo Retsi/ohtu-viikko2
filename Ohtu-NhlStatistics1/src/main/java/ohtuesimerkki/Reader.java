ec743edf94f9c3e4a34acc71ebf70ca7db99690f
Retsi <lauri.sainio@gmail.com>
2013-03-22 16:28:53 +0200

    tehtävä 2

---
+++ Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Reader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.List;
+
+/**
+ *
+ * @author lmsainio
+ */
+public interface Reader {
+    
+    public List<Player> getPlayers();
+    
+    
+}
